b1e463b3e4bec2f0b0ce64c6bfccb255d249d9b3
Natarajan <Anand_S_Natarajan@apac.dell.com>
2017-06-16 11:01:25 +0530

    Create Lists sample

---
+++ Lists.py
@@ -0,0 +1,10 @@
+# Create a new list
+my_list = [1,2,3,4]
+print(my_list)
+del my_list
+
+# Create a new list with heterogenous elements
+my_list = [1, "chennai", "delhi"]
+print(my_list)
+del my_list
+
